549477ed7fce41b2a5939d6cf51f606fb965450f
Jem Herrera <jhneramuz@gmail.com>
2022-07-29 12:45:15 -0600

    .

--- src/scripts/plano_sun/plano-sun.js
+++ src/scripts/plano_sun/plano-sun.js
@@ -6,14 +6,5 @@ const PlanoSunComponent = {
         return {
           state
         }
-    },
-    data() {
-        return {
-            message: 'Hello Vue!',
-            message2: 'hello hello hellos'
-        }
-    },
-    mounted(){
-        console.log(sunglasses, state.count)
     }
 }
