d3396ac9043626723aba925be899119af7205dee
Jem Herrera <jhneramuz@gmail.com>
2022-07-29 14:51:04 -0600

    .

--- src/scripts/plano_sun/plano-sun.js
+++ src/scripts/plano_sun/plano-sun.js
@@ -1,9 +1,13 @@
 
 const { reactive } = Vue
+
 const PlanoSunComponent = {
     name: "Plano Sunglasses",
     setup(){
         const state = reactive({ count: 0 })
+
+        const library = pageData;
+        console.log(library)
         return {
           state
         }
