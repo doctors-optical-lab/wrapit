45ff2d95cf33936bc3989800a2d4394b85a9a4bb
Jem Herrera <jhneramuz@gmail.com>
2022-08-05 15:00:33 -0600

    .

--- src/scripts/sunglasses/components/sunglasses-filter-modal.js
+++ src/scripts/sunglasses/components/sunglasses-filter-modal.js
@@ -4,13 +4,20 @@ const FilterModal = {
     template: `
     <div v-if="active" id="filter-modal">
         <h2>Filter Frames</h2>
+        <button class="close-modal" @click="active=false">X</button>
         <p>Don’t know the model of your frame? Let us guide you step by step.</p>
 
         <div class="frame-material-container">
-        
+            <h3>Step 1: Select your frame material</h3>
+            <ul class="selections">
+                <li class="selection" v-for="material in filters['Frame Material'].values>$%material%$</li>
+            </ul>
         </div>
         <div class="frame-shape-modal">
-
+            <h3>Step 1: Select your frame shape</h3>
+            <ul class="selections">
+                <li class="selection" v-for="shape in filters['Frame Shape'].values>$%shape%$</li>
+            </ul>
         </div>
     </div>
     `,
