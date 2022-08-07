9574d9144295addda2defadcc4e16e6dbe759c3f
Jem Herrera <jhneramuz@gmail.com>
2022-08-07 15:46:10 -0600

    .

--- assets/sunglasses-collection.js
+++ assets/sunglasses-collection.js
@@ -6,17 +6,19 @@ const SunglassesCollectionComponent = {
 		const filterModal = ref(false);
 
         function selectFrame(event){
-			window.location.href = createFilterUrl(collection.filters['Catalog Title']['parameter-name'], event)
+			window.location.href = `?${createFilterUrl(collection.filters['Catalog Title']['parameter-name'], event)}`
 		}
 
 		function createFilterUrl(parameter, value){
-			return `?${parameter}=${value.replace(' ', '+')}`
+			return `${parameter}=${value.replace(' ', '+')}`
 		}
 
-		function filterFrames(event){
-			console.log(event);
+		function filterFrames(activeFilters){
+			let materialURL = createFilterUrl(collection.filters['Frame Material']['parameter-name'], activeFilters.material),
+				shapeURL = createFilterUrl(collection.filters['Frame Shape']['parameter-name'], activeFilters.shape);
+			
+			window.location.href = `?${materialURL}&${shapeURL};`
 		}
-
         return {
           collection, filterModal, selectFrame, createFilterUrl, filterFrames
         }
