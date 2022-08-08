ed8ff4a825076134fd2740559871b7eba849638a
Jem Herrera <jhneramuz@gmail.com>
2022-08-07 18:26:55 -0600

    .

--- src/scripts/sunglasses/sunglasses-collection.js
+++ src/scripts/sunglasses/sunglasses-collection.js
@@ -20,11 +20,10 @@ const SunglassesCollectionComponent = {
 		function clearFilters(){
 			activeFilters.material = '';
 			activeFilters.shape = '';
-			console.log('clear')
 		}
 		//CHANGE URL BASED ON FILTERS
 		watch(activeFilters, (values) => {
-			if(!values.material || !values.shape) window.location.href = '';
+			if(!values.material || !values.shape) window.location.href = '?';
 			else{
 				let materialURL = createFilterUrl(collection.filters['Frame Material']['parameter-name'], values.material),
 					shapeURL = createFilterUrl(collection.filters['Frame Shape']['parameter-name'], values.shape);
