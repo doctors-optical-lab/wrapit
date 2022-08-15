61bfab5ea08674c4f044f96438b1dcf24c26364b
Jem Herrera <jhneramuz@gmail.com>
2022-08-15 16:57:30 -0600

    .

--- src/scripts/sunglasses/sunglasses-product.js
+++ src/scripts/sunglasses/sunglasses-product.js
@@ -2,12 +2,13 @@ const SunglassesProductComponent = {
     name: 'Sunglasses Product Component',
     delimiters: ["$%","%$"],
     setup(){
-        const product = reactive(productObject)
+        const product = reactive(productObject);
         const colors = computed(() => {
             let output = {};
 
             product.options['Lens Color'].forEach(color => {
-                let sampleVariant = product.variants.find(variant => variant.options.contains(color));
+                const sampleVariant = product.variants.find(variant => variant.options.includes(color));
+                if(color in output) return;
                 output[color] = {
                     "Frame Image": sampleVariant.metafields['Frame Image'],
                     "Human Image": sampleVariant.metafields['Human Image'],
@@ -16,12 +17,17 @@ const SunglassesProductComponent = {
                 }
             })
 
-            return 'hello'
             return output
 
             // Example output
         })
 
+        //GET the CURRENT ACTIVE VARIANT
+        const activeOptions = reactive({
+            'Option 1': product.variants[0].options[0],
+            'Option 2': product.variants[0].options[1],
+            'Option 3': product.variants[0].options[2]
+        })
         return {
             product, colors
         }
