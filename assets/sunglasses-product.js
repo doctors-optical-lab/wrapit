caac5c15141230c317ad62ac01df2d472b130872
Jem Herrera <jhneramuz@gmail.com>
2022-08-15 16:23:06 -0600

    .

--- assets/sunglasses-product.js
+++ assets/sunglasses-product.js
@@ -3,9 +3,27 @@ const SunglassesProductComponent = {
     delimiters: ["$%","%$"],
     setup(){
         const product = reactive(productObject)
+        const colors = computed(() => {
+            let output = {};
+
+            product.options['Lens Color'].forEach(color => {
+                let sampleVariant = product.variants.find(variant => variant.options.contains(color));
+                output[color] = {
+                    "Frame Image": sampleVariant.metafields['Frame Image'],
+                    "Human Image": sampleVariant.metafields['Human Image'],
+                    "Mirror Color Type": sampleVariant.metafields['Mirror Color Type'],
+                    "Orb Image": sampleVariant.metafields['Orb Image']
+                }
+            })
+
+            return 'hello'
+            return output
+
+            // Example output
+        })
 
         return {
-            product
+            product, colors
         }
     }
 }
